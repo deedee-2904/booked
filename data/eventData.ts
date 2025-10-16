58364600494a02695fd4582fec75b553123d7f03
deedee-2904 <demaradarkwah@mail.com>
2025-10-16 18:06:37 +0100

    A clickable event which routes to an event page where users can sign up for the event

--- data/eventData.ts
+++ data/eventData.ts
@@ -5,6 +5,7 @@ export interface EventCardProps {
 		event_date_time: string;
 		event_description: string;
 		book_id: string;
+		signed_up:boolean;
 	};
 	book?:any;
 }
@@ -17,6 +18,7 @@ export const events = [
 		event_description:
 			"Meet the author of the best-selling vampire book series and get books signed.",
 		book_id: "lGjFtMRqp_YC",
+		signed_up:false,
 	},
 	{
 		event_id: 2,
@@ -25,6 +27,7 @@ export const events = [
 		event_description:
 			"Get your copy of the highly anticipated sequel to Bride by Ali Hazelwood, freebies are limited.",
 		book_id: "ztdIEQAAQBAJ",
+		signed_up:false,
 	},
 	{
 		event_id: 3,
@@ -32,5 +35,6 @@ export const events = [
 		event_date_time: "09-01-2026",
 		event_description: "Come and watch the Netflix movie adaptaion with us!",
 		book_id: "xWNjEAAAQBAJ",
+		signed_up:false,
 	},
 ];
