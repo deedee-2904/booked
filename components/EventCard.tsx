1e6f388e7ce79905f1f5a0eba91929fb80c8f969
deedee-2904 <demaradarkwah@mail.com>
2025-10-26 12:06:46 +0000

    Updated event card styling

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -3,6 +3,8 @@ import { EventCardProps } from "@/data/eventData";
 import { Link } from "expo-router";
 import { Pressable, StyleSheet, Text } from "react-native";
 import { Card } from "./ui/card";
+import { HStack } from "./ui/hstack";
+import { VStack } from "./ui/vstack";
 
 export default function EventCard({ book, event }: EventCardProps) {
 	const thumbnail = book?.volumeInfo?.imageLinks?.thumbnail || event.book_thumbnail;
@@ -16,31 +18,37 @@ export default function EventCard({ book, event }: EventCardProps) {
 				<Card
 					style={{ alignSelf: "center", backgroundColor: "#332e2eff" }}
 					key={event.event_id}
-					className="p-5 rounded-lg w-[90%] max-w-[400px] m-3"
+					className="p-5 rounded-lg max-w-[360px] m-3"
 					variant={"filled"}
 				>
-					<Text style={{ fontSize: 25, fontWeight: "bold", color: "#fff" }}>
-						{event.event_title}{" "}
-					</Text>
-					<Text style={styles.datetime}>
-						{event.event_date_time.toLocaleDateString([], {
-							day: "numeric",
-							month: "short",
-							year: "numeric",
-						})}{" "}
-						at{" "}
-						{event.event_date_time.toLocaleTimeString([], {
-							hour: "2-digit",
-							minute: "2-digit",
-						})}
-					</Text>
-					{book && book.volumeInfo?.imageLinks?.thumbnail && (
-						<Image
-							source={{ uri: thumbnail }}
-							className=" h-[150px] w-[100px] rounded-md aspect-[150/240]"
-							alt={`Book Cover for ${book.volumeInfo.title} by ${book.volumeInfo.authors}`}
-						/>
-					)}
+					<HStack reversed={true} className="items-center w-full space-x-4" space="lg">
+						<VStack className="items-left" style={{ flexGrow:1 }}>
+					
+							<Text style={{ fontSize: 25, fontWeight: "bold", color: "#fff" }}>
+								{event.event_title}
+							</Text>
+							<Text style={styles.datetime}>
+								{event.event_date_time.toLocaleDateString([], {
+									day: "numeric",
+									month: "short",
+									year: "numeric",
+								})}{" "}
+								at{" "}
+								{event.event_date_time.toLocaleTimeString([], {
+									hour: "2-digit",
+									minute: "2-digit",
+								})}
+							</Text>
+						</VStack>
+
+						{book && book.volumeInfo?.imageLinks?.thumbnail && (
+							<Image
+								source={{ uri: thumbnail }}
+								className=" h-[150px] w-[100px] rounded-md aspect-[150/240]"
+								alt={`Book Cover for ${book.volumeInfo.title} by ${book.volumeInfo.authors}`}
+							/>
+						)}
+					</HStack>
 				</Card>
 			</Pressable>
 		</Link>
